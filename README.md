d539457088c04b7057ad05f3f00e2170284072e0
Shushant Ghosh <ghoshshushant00@gmail.com>
2025-09-24 22:27:51 -0700

    Setup.py and dvc

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # insurance_premium_ML
+python -m venv myenv
+myenv\Scripts\activate
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Insurance_MLOps',
+    version= '0.0.0',
+    author= 'Shushant',
+    author_email= 'shushantghosh.data@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
